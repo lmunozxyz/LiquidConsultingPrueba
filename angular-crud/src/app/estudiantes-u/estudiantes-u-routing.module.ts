ab5ef34bcb3e83879240a10901c1fee75457bd3f
Luis Munoz <lmunoz.xyz@gmail.com>
2020-11-02 18:56:12 -0600

    Version con todos los componentes y estable

--- angular-crud/src/app/estudiantes-u/estudiantes-u-routing.module.ts
+++ angular-crud/src/app/estudiantes-u/estudiantes-u-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { EstudiantesUComponent } from './estudiantes-u.component';
+import { CreateEstudianteComponent } from './create-estudiante/create-estudiante.component';
+import { UpdateEstudianteComponent } from './update-estudiante/update-estudiante.component';
+import { DeleteEstudianteComponent } from './delete-estudiante/delete-estudiante.component';
+import { ViewEstudianteComponent } from './view-estudiante/view-estudiante.component';
+import { ViewAllEstudianteComponent } from './view-all-estudiante/view-all-estudiante.component';
+import { ViewAllEstudianteByCategoryComponent } from './view-all-estudiante-by-category/view-all-estudiante-by-category.component';
+import { ViewAllEstudianteByDateComponent } from './view-all-estudiante-by-date/view-all-estudiante-by-date.component';
 
-const routes: Routes = [{ path: '', component: EstudiantesUComponent }];
+const routes: Routes = [
+  { path: '', component: EstudiantesUComponent },
+  { path: 'create-estudiante', component: CreateEstudianteComponent },
+  { path: 'update-estudiante', component: UpdateEstudianteComponent },
+  { path: 'delete-estudiante', component: DeleteEstudianteComponent },
+  { path: 'view-estudiante', component: ViewEstudianteComponent },
+  { path: 'view-all-estudiante', component: ViewAllEstudianteComponent },
+  { path: 'view-all-estudiante-by-category', component: ViewAllEstudianteByCategoryComponent },
+  { path: 'view-all-estudiante-by-date', component: ViewAllEstudianteByDateComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
