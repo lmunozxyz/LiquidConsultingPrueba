9e229b7c1fd8754c061d671d0281e8d64e744932
Luis Munoz <lmunoz.xyz@gmail.com>
2020-11-04 11:01:53 -0600

    Se agrego el modulo de borrar y funciona correctamente

--- angular-crud/src/app/estudiantes-u/delete-estudiante/delete-estudiante.component.ts
+++ angular-crud/src/app/estudiantes-u/delete-estudiante/delete-estudiante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EstudiantesService } from '../estudiantes.service';
 
 @Component({
   selector: 'app-delete-estudiante',
@@ -10,11 +11,19 @@ export class DeleteEstudianteComponent implements OnInit {
 
   estudianteId = 0;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private estudianteService: EstudiantesService
+    ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(data=> {
+    this.activatedRoute.params.subscribe(data => {
       this.estudianteId = data.id;
+
+
+      this.estudianteService.deleteEstudiante(this.estudianteId).subscribe(deleteEstudiantedata => {{
+        console.log('Estudiante borrado');
+      }})
     });
   }
 
